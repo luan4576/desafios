325a207b0dc5060682ee807703fa9aad2348bb0c
luan4576 <luansouza2002.ls@gmail.com>
2019-10-31 10:03:19 -0300

    novo ex

---
+++ IMC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("digite seu peso");
+            int pa=int.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine("digite sua altura");
+            int pa=int.Parse(Console.ReadLine());
+            
+            
+            
+        }
+    }
+}
--- inteiro1A12/Program.cs
+++ inteiro1A12/Program.cs
@@ -1,22 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace inteiro1A12
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("escreva um numero de 1 a 12:");
-            int a = int.Parse(Console.ReadLine());
+namespace inteiro1A12 {
+    class Program {
+        static void Main (string[] args) {
+
+            Console.WriteLine ("escreva um numero de 1 a 12:");
+            int a = int.Parse (Console.ReadLine ());
+
             
-
-            for(int i=0;i <=12;i++){
-                switch(i){
+                switch (a) {
                     case 1:
-                    1=janeiro
-                    
+                        System.Console.WriteLine ($"janeiro");
+                        break;
+                    case 2:
+                        System.Console.WriteLine ($"fevereiro");
+                        break;
+                    case 3:
+                        System.Console.WriteLine ($"março");
+                        break;
+                    case 4:
+                        System.Console.WriteLine ($"abril");
+                        break;
+                    case 5:
+                        System.Console.WriteLine ($"maio");
+                        break;
+                    case 6:
+                        System.Console.WriteLine ($"junho");
+                        break;
+                    case 7:
+                        System.Console.WriteLine ($"julho");
+                        break;
+                    case 8:
+                        System.Console.WriteLine ($"agosto");
+                        break;
+                    case 9:
+                        System.Console.WriteLine ($"setembro");
+                        break;
+                    case 10:
+                        System.Console.WriteLine ($"outubro");
+                        break;
+                    case 11:
+                        System.Console.WriteLine ($"novembro");
+                        break;
+                    case 12:
+                        System.Console.WriteLine ($"dezembro");
+                        break;
                 }
+
             }
+
         }
     }
-}
